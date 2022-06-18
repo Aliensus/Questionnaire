db378f4010dc039a6e7542ef682bb934a9f1ef6f
Alena Susloparova <susloparova.alena@gmail.com>
2022-06-18 18:52:59 +0400

    Добавлены надписи

--- app/src/main/java/com/diploma/questionnaire/presentation/MainActivity.kt
+++ app/src/main/java/com/diploma/questionnaire/presentation/MainActivity.kt
@@ -2,15 +2,27 @@ package com.diploma.questionnaire.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.diploma.questionnaire.R
 import com.diploma.questionnaire.data.Data.questions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var testHeaderTv : TextView
+    private lateinit var questionHeaderTv : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testHeaderTv = findViewById(R.id.testHeaderTv)
+        //testHeaderTv.text = "Заголовок вопроса"
+        questionHeaderTv = findViewById(R.id.questionHeaderTv)
+        //questionHeaderTv.text = "Текст вопроса"
+
         val questions = questions.map {it ->
-            it.questionContent.uppercase() + R.string.app_name
+            it.questionContent + R.string.app_name
+            questionHeaderTv.text = it.questionContent
         }
+
     }
+
 }
